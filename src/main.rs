c714d77518ebead21c0f75b39665432981ee1d52
rRupeshRanjan <rupeshranjan409@gmail.com>
2025-10-29 11:20:21 +0000

    Add comments to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-#![no_std]
-#![no_main]
+#![no_std] // dont link Rust standard library
+#![no_main] // disable all Rust leevle entry points
+
 use core::panic::PanicInfo;
 
+// This function is called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[unsafe(no_mangle)]
+#[unsafe(no_mangle)] // dont mangle the name of this function
 pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a fucntion
+    // named _start by default
     loop {}
 }
