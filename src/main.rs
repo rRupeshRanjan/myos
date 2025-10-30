f7926c22c93f995c8a6517cd132b21a4c7bcba5c
rRupeshRanjan <rupeshranjan409@gmail.com>
2025-10-30 11:49:52 +0000

    Print test failure message to console

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 
 // This function is called on panic
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
@@ -45,6 +46,16 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
     serial_println!("Running {} tests", tests.len());
@@ -58,6 +69,6 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
 #[test_case]
 fn trivial_assertion() {
     serial_print!("trivial assertion... ");
-    assert_eq!(1, 1);
+    assert_eq!(1, 2);
     serial_println!("[ok]");
 }
